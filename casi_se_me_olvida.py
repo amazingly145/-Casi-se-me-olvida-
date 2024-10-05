7ea774f474e80e14356890e6b789fd4f41142496
amazingly145 <A01753419@tec.mx>
2024-10-04 20:16:53 -0600

    Update casi_se_me_olvida.py

--- casi_se_me_olvida.py
+++ casi_se_me_olvida.py
@@ -1,17 +1,28 @@
 """La función prueba:
+Podremos visualizar las funciones que se
+puedan colocar números, de esta manera
+podemos visualizar algunos ejemplos
+de lo que saldría de la función"""
+def pruebas():
+    resultado=conversion_k(5)
+    print(resultado)
+    resultado=conversion_l(5)
+    print(resultado)
+"""La función lista antes:
 es la función para crear la lista del súper, donde el usuario
 podrá colocar sus artículos, haciendo su lista, posteriormente
 se podrá agregar la función de listas para que se vayan guardando
 los artículos y el usuario pueda ver de nuevo su lista creada,
 podrá modificarla y ver las listas que había hecho anteriormente"""
-def lista_antes ():
+def lista_antes (lista_compras):
     contador=0
-    entrada_salida=str(input("Ingresa N si deseas cerrar la lista, ingresa S si deseas continuar de la lista: "))
+    entrada_salida="S"
     while entrada_salida=="S":
-        lista_compras=str(input("Ingresa el nombre del artículo"))
-        entrada_salida=str(input("Ingresa N si deseas cerrar la lista, ingresa S si deseas continuar de la lista"))
+        articulo=str(input("Ingresa el nombre del artículo: "))
+        lista_compras.append((articulo))
+        entrada_salida=str(input("Ingresa N si deseas cerrar la lista, ingresa S si deseas continuar de la lista: "))
         contador+=1
-    print("Ingresa N si deseas regresar al menú principal")
+    return(lista_compras)
 """La función precio_productos:
 es la función donde el usuario, ponga un valor máximo a gastar.
 Cuando se encuentre en la tienda, podrá poner el artículo, el costo y
@@ -19,20 +30,29 @@ el número a llevar del mismo, al final podrá ver cuánto
 se gastó y el número de asrtículos que lleva. También se guardará en otra lista,
 se podrá ver los artículos, junto con su precio y el total a gastar
 del usuario."""
-def precios_productos():
-    monto_max=int(input("Ingresa el $monto$, máximo a gastar: "))
-    numero_productos_total=int(input("Ingresa el número de artículos a comprar: "))
-    entrada_salida="S"
+def precios_productos(matriz,monto_max,numero_productos_total):
     resultado=0
     contador=0
     print("El monto máximo a gastar es de: "+"$"+str(monto_max))
-    for x in range (numero_productos_total):
-        nombre_productos=str(input("Ingresa el nombre del artículo: "))
-        numero_productos=int(input("¿Cuántos "+str(nombre_productos)+" vas a comprar?"))
-        precio_productos=int(input("Ingresa el precio del artículo: "))
-        entrada_salida=str(input("Ingresa N si deseas ver el resumen, S si deseas continuar con la lista: "))
-        resultado+=(numero_productos*precio_productos)
-        contador+=numero_productos
+    for a in range(numero_productos_total):
+        producto=[]
+        nombre_producto=input("Ingresa el nombre del artículo: ")
+        numero_producto=int(input("¿Cuántos "+nombre_producto+ " vas a comprar? "))
+        producto.append(numero_producto)
+        producto.append(nombre_producto)
+        precio_producto=int(input("Ingresa el precio del artículo: "))
+        producto.append(precio_producto)
+        matriz.append(producto)
+        resultado+=(numero_producto*precio_producto)
+        contador+=numero_producto
+    print("---------------------Resumen de compras---------------------")
+    print("número de artículos    "+"nombre productos   "+"precio de artículo   ")
+    print("------------------------------------------------------------")
+    for b in range(len(matriz)):
+        for c in range(len(matriz[b])):
+            print("      ",matriz[b][c],"           ", end='')
+        print()
+    print("¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨")
     if monto_max<=resultado:
         print("Te has pasado con el monto inicial de tú lista")
     else:
@@ -62,22 +82,29 @@ def menu():
     print("2. Visualzar lista")
     print("3. Ingresar productos y precios")
     print("4. Conversión de unidades de kilogramos a libras")
-    print("5. Salir")
+    print("5. Prueba")
+    print("6. Salir")
 """La función main:
 Dependiendo de la selección del usuario,
 podrá hacerse las diferentes funciones ya establecidas.""" 
 def main():
+    lista_compras=[]
+    matriz_principal=[]
+    matriz=[]
     while True:
         menu()
         usuario=int(input("Favor de ingresar el número de acuerdo a la acción a realizar: "))
         if usuario==1:
             print("Bienvenido a la creación de tú lista de super")
-            lista_antes()
+            matriz_principal.append(lista_antes(lista_compras))
         if usuario==2:
-            print("En proceso de creación con listas, muchas de las funciones, aún le falta arreglos")
+            print("----Lista de compras----")
+            print(matriz_principal)
         if usuario==3:
             print("Bienvenido a tú lista de super en tiempo real")
-            precios_productos()
+            monto_max=int(input("Ingresa el $monto$, máximo a gastar: "))
+            numero_productos_total=int(input("Ingresa el número de artículos a comprar: "))
+            precios_productos(matriz,monto_max,numero_productos_total)
         if usuario==4:
             print("Bienvenido a la sección de conversión de unidades")
             usuario_1=str(input("Si deseas convertir de kilogramos a libras ingresa kl, si es lo contrario ingresa lk: "))
@@ -90,12 +117,8 @@ def main():
                 kilogramos=conversion_l(p)
                 print(str(p)+"lb es igual a: "+str(kilogramos)+"kg")
         if usuario==5:
+            pruebas()  
+        if usuario==6:
             print("Gracias por utilizar ¡Casi se me olvida!")
-            break        
+            break      
 main()
-"""def pruebas():
-    resultado=conversion_k(5)
-    print(resultado)
-    resultado=conversion_l(5)
-    print(resultado)
-pruebas()"""
